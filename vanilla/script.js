f067ede3b77fde355b5ce1883343580e7b49d032
Mateus Pereira Bittencourt <mateuspbitt@gmail.com>
2023-07-24 12:41:34 -0300

    The files that will receive the training codes were created.

--- vanilla/script.js
+++ vanilla/script.js
@@ -1,18 +1,18 @@
 const toDoList = [
-  { id: 1, title: "Nullsh Coalescing Operator" },
-  { id: 2, title: "Objetos" },
-  { id: 3, title: "Desestruturação" },
-  { id: 4, title: "Rest Operator" },
-  { id: 5, title: "Optional Chaining" },
-  { id: 6, title: "Map()" },
-  { id: 7, title: "Filter()" },
-  { id: 8, title: "Every()" },
-  { id: 9, title: "Some()" },
-  { id: 10, title: "Find e FindInder()" },
-  { id: 11, title: "Reduce()" },
-  { id: 12, title: "Template Literals" },
-  { id: 13, title: "Promises" },
-  { id: 14, title: "Import e Export" },
+  { id: 1, title: "Nullsh Coalescing Operator", link: "nullshCoalescing" },
+  { id: 2, title: "Objetos", link: "objects" },
+  { id: 3, title: "Desestruturação", link: "destructuring" },
+  { id: 4, title: "Rest Operator", link: "restOperator" },
+  { id: 5, title: "Optional Chaining", link: "optionalChaining" },
+  { id: 6, title: "Map()", link: "map" },
+  { id: 7, title: "Filter()", link: "filter" },
+  { id: 8, title: "Every()", link: "every" },
+  { id: 9, title: "Some()", link: "some" },
+  { id: 10, title: "Find e FindInder()", link: "findAndFindindex" },
+  { id: 11, title: "Reduce()", link: "reduce" },
+  { id: 12, title: "Template Literals", link: "templateLiterals" },
+  { id: 13, title: "Promises", link: "promises" },
+  { id: 14, title: "Import e Export", link: "importsAndExport" },
 ]
 
 const listContainer = document.querySelector("#list")
@@ -25,7 +25,7 @@ function renderList(toDoList) {
   } else {
     toDoList.forEach((toDoItem, index) => {
       list += `
-      <div id="itemLista"><a href="#">&#x1F4A5 ${toDoItem.title} &#x1F4A5</a></div>
+      <div id="itemLista"><a href="pages/${toDoItem.link}/index.html">&#x1F4A5 ${toDoItem.title} &#x1F4A5</a></div>
       `
     })
   }
