73334bd7b39c144bcc6da02da51f601323a1f02e
Mateus Pereira Bittencourt <mateuspbitt@gmail.com>
2023-08-09 12:26:03 -0300

    created new func getPopular to get tv shows in API

--- bittFlix/apiRequest.js
+++ bittFlix/apiRequest.js
@@ -1,28 +1,26 @@
+import { showMovie, showTv } from "./src/script.js"
+
 async function getcontent() {
   try {
-    const response = await fetch("http://localhost:4567/")
+    const response = await fetch("http://localhost:4567/popular")
     const data = await response.json()
     console.log(data)
-    showDate(data)
+    showMovie(data)
   } catch (error) {
     console.log(error)
   }
 }
 
-getcontent()
-
-function showDate(movies) {
-  let output = ""
-
-  for (let i = 0; i < 5; i++) {
-    let title = movies.results[i]
-
-    output += `<div id="filmCard">
-                <img id="movieImg" src="https://image.tmdb.org/t/p/w500/${title.poster_path}" alt="">
-                <div id="voteAverage"><p>${title.vote_average}</p></div>
-                <p>${title.original_title}</p>
-                <button>Detalhes</button>
-               </div>`
+async function getPopular() {
+  try {
+    const response = await fetch("http://localhost:4567/tv/popular")
+    const popular = await response.json()
+    console.log(popular)
+    ///////////////
+  } catch (error) {
+    console.log(error)
   }
-  document.getElementById("midContainer").innerHTML = output
 }
+
+getPopular()
+getcontent()
