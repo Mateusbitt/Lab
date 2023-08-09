503c48b29c1bdda76e976292941d82094986dadc
Mateus Pereira Bittencourt <mateuspbitt@gmail.com>
2023-08-09 12:21:41 -0300

    creat new api rout /tv/popular

--- bittFlixBack/server.js
+++ bittFlixBack/server.js
@@ -5,10 +5,21 @@ const app = express()
 
 app.use(cors())
 
-app.get("/", async (req, res) => {
+app.get("/popular", async (req, res) => {
   try {
     const { data } = await axios(
       "https://api.themoviedb.org/3/movie/popular?api_key=fc7d295c26d9108d9d70c770ee1064b7"
+    )
+    return res.json(data)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.get("/tv/popular", async (req, res) => {
+  try {
+    const { data } = await axios(
+      "https://api.themoviedb.org/3/tv/popular?api_key=fc7d295c26d9108d9d70c770ee1064b7"
     )
     return res.json(data)
   } catch (error) {
