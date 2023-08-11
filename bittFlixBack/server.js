29405acf9c07cdea6562b63a1ede676a8268c848
Mateus Pereira Bittencourt <mateuspbitt@gmail.com>
2023-08-11 13:12:18 -0300

    routes for requests from api

--- bittFlixBack/server.js
+++ bittFlixBack/server.js
@@ -16,14 +16,26 @@ app.get("/popular", async (req, res) => {
   }
 })
 
-app.get("/tv/popular", async (req, res) => {
+app.get("/movie/top_rated", async (req, res) => {
   try {
     const { data } = await axios(
-      "https://api.themoviedb.org/3/tv/popular?api_key=fc7d295c26d9108d9d70c770ee1064b7"
+      "https://api.themoviedb.org/3/movie/top_rated?api_key=fc7d295c26d9108d9d70c770ee1064b7"
     )
     return res.json(data)
   } catch (error) {
     console.log(error)
+  }
+})
+
+app.get("/movie/:movieId", async (req, res) => {
+  try {
+    const movieId = req.params.movieId;
+    const { data } = await axios(
+      `https://api.themoviedb.org/3/movie/${movieId}?api_key=fc7d295c26d9108d9d70c770ee1064b7`
+    );
+    return res.json(data);
+  } catch (error) {
+    console.log(error);
   }
 })
 
